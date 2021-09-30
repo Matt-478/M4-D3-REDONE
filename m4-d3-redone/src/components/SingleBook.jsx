e540753a0e20a2fb055060a8ff6feb32e87d5661
Matīss Komļevs <matisskomlevs@gmail.com>
2021-09-30 16:49:19 +0300

    Created SingleBook component

--- m4-d3-redone/src/components/SingleBook.jsx
+++ m4-d3-redone/src/components/SingleBook.jsx
@@ -1,12 +1,12 @@
-// import Card from 'react-bootstrap/Card'
+import Card from 'react-bootstrap/Card'
 
-// const SingleBook = ({ book }) => (
-//   <Card >
-//   <Card.Img variant="top" src={book.img} />
-//   <Card.Body>
-//     <Card.Title>{book.title}</Card.Title>
-//   </Card.Body>
-// </Card>
-// )
+const SingleBook = ({ book }) => (
+  <Card >
+  <Card.Img variant="top" src={book.img} />
+  <Card.Body>
+    <Card.Title>{book.title}</Card.Title>
+  </Card.Body>
+</Card>
+)
 
-// export default SingleBook
+export default SingleBook
