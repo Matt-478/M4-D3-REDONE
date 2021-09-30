f62dcb6d7cb59accd2b7679b87acafd031bc5705
Matīss Komļevs <matisskomlevs@gmail.com>
2021-09-30 17:50:21 +0300

    Implemented FilterBookList function

--- m4-d3-redone/src/components/BookList.jsx
+++ m4-d3-redone/src/components/BookList.jsx
@@ -1,19 +1,46 @@
-import {Container, Col, Row} from 'react-bootstrap'
+import React from 'react'
+import {Container, Col, Row, Form} from 'react-bootstrap'
 import SingleBook from './SingleBook'
 
-const BookList = (props) => (
-  <Container>
-    <Row>
-      {
-        props.books.map(b => (
-          <Col xs={3}>
-            <SingleBook book={b}/>
-          </Col>
+class BookList extends React.Component {
+
+  state = {
+    searchQuery: ''
+  }
+
+  render() {
+    return (
+
+    <Container>
+      <Row>
+        <Col>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Search</Form.Label>
+            <Form.Control 
+            type="text" 
+            placeholder="Search here"
+            value={this.state.searchQuery}
+            onChange={e => this.setState({searchQuery: e.target.value})}
+             />
+          </Form.Group>
+        </Form>
+        </Col>
+      </Row>
+
+      <Row>
+        {
+          this.props.books.filter(b => b.title.toLowerCase().includes(this.state.searchQuery)).map(b => (
+            <Col xs={3}>
+              <SingleBook book={b}/>
+            </Col>
+            )
           )
-        )
-      }
-    </Row>
-  </Container>
-)
+        }
+      </Row>
+    </Container>
+    )
+  }
+}
 
 export default BookList
--- m4-d3-redone/src/components/SingleBook.jsx
+++ m4-d3-redone/src/components/SingleBook.jsx
@@ -4,7 +4,7 @@ const SingleBook = ({ book }) => (
   <Card >
   <Card.Img variant="top" src={book.img} />
   <Card.Body>
-    <Card.Title>{book.title}</Card.Title>
+    <Card.Title style={{color: "black" }}>{book.title}</Card.Title>
   </Card.Body>
 </Card>
 )
