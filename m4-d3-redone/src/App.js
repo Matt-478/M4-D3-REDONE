2409b7183863c1eb2534972f6f3dfaa3ef46098d
Matīss Komļevs <matisskomlevs@gmail.com>
2021-09-30 16:48:56 +0300

    Created MyBadge component

--- m4-d3-redone/src/App.js
+++ m4-d3-redone/src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 // import './components/WarningSign'
 import WarningSign from './components/WarningSign';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MyBadge from './components/MyBadge'
+import SingleBook from './components/SingleBook'
+import fantasyBooks from './fantasy.json'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <WarningSign  text="Here's a warning"/>
+        <MyBadge color="success" text="A little surprise"/>
+        <SingleBook book={fantasyBooks[0]}/>
       </header>
     </div>
   );
---
+++ m4-d3-redone/src/components/SingleBook.jsx
@@ -0,0 +1,12 @@
+// import Card from 'react-bootstrap/Card'
+
+// const SingleBook = ({ book }) => (
+//   <Card >
+//   <Card.Img variant="top" src={book.img} />
+//   <Card.Body>
+//     <Card.Title>{book.title}</Card.Title>
+//   </Card.Body>
+// </Card>
+// )
+
+// export default SingleBook
