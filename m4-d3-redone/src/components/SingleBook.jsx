35d93b6e81bb209eb8973f0e61999229e817ab75
Matīss Komļevs <matisskomlevs@gmail.com>
2021-09-30 17:59:36 +0300

    Selected state changes card visually

--- m4-d3-redone/src/components/SingleBook.jsx
+++ m4-d3-redone/src/components/SingleBook.jsx
@@ -1,12 +1,24 @@
+import React from 'react'
 import Card from 'react-bootstrap/Card'
 
-const SingleBook = ({ book }) => (
-  <Card >
-  <Card.Img variant="top" src={book.img} />
-  <Card.Body>
-    <Card.Title style={{color: "black" }}>{book.title}</Card.Title>
-  </Card.Body>
-</Card>
-)
+class SingleBook extends React.Component {
+
+  state = {
+    selected: false
+  }
+  render(){
+    return(
+       <Card 
+       onClick={() => this.setState({selected: !this.state.selected})}
+       style={{border: this.state.selected ? "3px solid blue" : "none"}}
+       >
+       <Card.Img variant="top" src={this.props.book.img} />
+       <Card.Body>
+         <Card.Title style={{color: "black" }}>{this.props.book.title}</Card.Title>
+       </Card.Body>
+      </Card>
+    )
+  }
+}
 
 export default SingleBook
